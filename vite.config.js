b54409147b643bb6ce0c7aba58eda7b8ab2f7245
Ekaterina Danilenko <keitilins@yandex.ru>
2024-11-29 17:14:10 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://ketrindan.github.io/Sidebar-html/",
   plugins: [react()],
 })
